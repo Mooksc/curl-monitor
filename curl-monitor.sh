15b412b8c32d657e847b7766e3a8d017aa0707e8
Mooks <mooks@localhost.localdomain>
2020-01-11 04:48:28 -0600

    improved string formatting and logging

--- curl-monitor.sh
+++ curl-monitor.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-url=$1
+URL=$1
 val0=""
-logCurlResponse(){
- tmpfile=$(mktemp) && tmpfile1=$(mktemp)
- curl --dump-header $tmpfile $url > $tmpfile1
- newVar=$(head -1 $tmpfile)
+getCurlResponse(){
+ tmpfile0=$(mktemp) && tmpfile1=$(mktemp)
+ curl --dump-header $tmpfile0 $URL > $tmpfile1
+ IFS=' ' && read -ra code <<< $(head -1 $tmpfile0)
  if [ "$val0" = "" ]; then
-   val0=${newVar#HTTP/1.1} && val1=6
- elif [ "$val0" != "${newVar#HTTP/1.1}" ]; then
-   val0=${newVar#HTTP/1.1} && val1=15
+   val0="${code[1]} ${code[2]}" && codePos=4 && counterPos=0
+ elif [ "$val0" != "${code[1]} ${code[2]}" ]; then
+   val0="${code[1]} ${code[2]}" && codePos=16 && counterPos=12
  fi
- tput cup 5 $val1 && echo $val0 
- tput cup 7 0 && head -1 $tmpfile1
 }
-
-val2=1
-while [ $val2 -le $2 ]; do 
- sleep 1 && tput cup 5 0 && echo $val2
- tput cup 1 0 && logCurlResponse && ((val2++))
+counter=1
+while [ $counter -le $2 ]; do
+ tput cup 1 0 && getCurlResponse
+ tput cup 5 $counterPos && echo $counter
+ tput cup 5 $codePos && echo $val0
+ tput cup 7 0 && head -1 $tmpfile1
+ sleep 1 && ((counter++))
 done
